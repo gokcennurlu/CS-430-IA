841ac7f6314e5d9088628af8d3d5f7627bdc5e16
Martin Hallen <hallen@icbc07pc36.epfl.ch>
2015-10-28 19:53:24 +0100

    Added and implemented AStar

--- deliberative/src/template/AStar.java
+++ deliberative/src/template/AStar.java
@@ -6,14 +6,14 @@ import java.util.PriorityQueue;
 import logist.simulation.Vehicle;
 import logist.task.TaskSet;
 
-public class Dijkstra extends GenericSearch {
+public class AStar extends GenericSearch {
 
-	public Dijkstra(Vehicle vehicle, TaskSet tasks, State startState) {
+	public AStar(Vehicle vehicle, TaskSet tasks, State startState) {
 		super(vehicle, tasks, startState);
 		this.queue = new PriorityQueue<State>(10, new Comparator<State>() {
 			@Override
 			public int compare(State o1, State o2) {
-				if (o1.g < o2.g) return -1;
+				if (o1.f() < o2.f()) return -1;
 				return 1;
 			}
 		});
