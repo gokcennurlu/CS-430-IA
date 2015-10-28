fbc3c98acd4c5d9f7ba6c5867123cccef33a8212
Martin Hallen <marthall@outlook.com>
2015-10-28 20:27:54 +0100

    Fixed bug in resultNode

--- deliberative/src/template/BFS.java
+++ deliberative/src/template/BFS.java
@@ -1,6 +1,8 @@
 package template;
 
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 import logist.simulation.Vehicle;
 import logist.task.TaskSet;
@@ -9,7 +11,13 @@ public class BFS extends GenericSearch {
 	
 	public BFS(Vehicle vehicle, TaskSet tasks, State startState) {
 		super(vehicle, tasks, startState);
-		this.queue = new LinkedList<State>();
+		this.queue = new PriorityQueue<State>(10, new Comparator<State>() {
+			@Override
+			public int compare(State o1, State o2) {
+				if (o1.LEVEL < o2.LEVEL) return -1;
+				return 1;
+			}
+		});
 	}
 
 	@Override
@@ -20,6 +28,7 @@ public class BFS extends GenericSearch {
 	@Override
 	protected void addToQueue(State element) {
 		this.queue.add(element);
+		
 	}
 
 	@Override
--- deliberative/src/template/GenericSearch.java
+++ deliberative/src/template/GenericSearch.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 
@@ -14,7 +15,7 @@ import logist.task.TaskSet;
 import logist.topology.Topology.City;
 
 public abstract class GenericSearch {
-	protected Queue<State> queue;
+	protected PriorityQueue<State> queue;
 	private Vehicle vehicle;
 	private TaskSet tasks;
 	private State startState;
@@ -36,6 +37,12 @@ public abstract class GenericSearch {
 		
 		while (!this.queue.isEmpty()){
 			State head = this.getNextFromQueue();
+			
+			if(head.isFinalState()) {
+				System.out.println("Number of states searched: " + states.size());
+				this.queue.clear();
+				return generatePlan(vehicle, head);
+			}
 //			System.out.print(head.g);
 //			System.out.print(" " + head.currentTasks.size());
 //			System.out.println(" " + head.remainingTasks.size());
@@ -54,16 +61,10 @@ public abstract class GenericSearch {
 					this.addToQueue(child);
 				} else {
 					// If the element is in the queue, but there is a shorter path there, update queue
-					if (head.currentCity.distanceTo(alreadyAddedState.currentCity) < alreadyAddedState.g) {
+					if (this.queue.comparator().compare(child, alreadyAddedState) < 0) {
 						this.removeFromQueue(alreadyAddedState);
 						this.addToQueue(child);
 					}
-					
-				}
-				if(child.isFinalState()) {
-					System.out.println("Number of states searched: " + states.size());
-					this.queue.clear();
-					return generatePlan(vehicle, child);
 					
 				}
 			}
@@ -89,12 +90,9 @@ public abstract class GenericSearch {
 
 		City currentCity = vehicle.getCurrentCity();
 		Plan plan = new Plan(currentCity);
-		
-		double sumDistance = 0;
 
 //		Create a list of all states in plan
 		while(current != null) {
-			sumDistance += current.currentCity.distanceTo(current.ancestor.currentCity);
 			stateList.add(0,current);
 			current = current.ancestor;
 		}
